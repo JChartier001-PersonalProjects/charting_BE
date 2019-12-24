d18ce08b2e40d72f355b678fc14168d804f0529e
Jen Chartier <54649817+JChartier001@users.noreply.github.com>
2019-12-24 16:32:24 -0600

    added node_modules to gitignore.  added auth router, db config,

--- api/server.js
+++ api/server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const authRouter = require('../auth/auth-router.js')
+
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+
+server.use('/api/auth', authRouter);
 
 
 module.exports = server;
