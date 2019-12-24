05f05c3e658c4d573ac0a986c15fc606e1ad1ed3
Jen Chartier <54649817+JChartier001@users.noreply.github.com>
2019-12-24 13:46:42 -0600

    added more dependencies, configured knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,22 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/charting.db3'
+    },
+    useNullAsDefault: true,
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done);
+    },
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: "./data/migrations"
+    
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  seeds: {
+    directory: "./data/seeds"
   }
-
+} 
 };
