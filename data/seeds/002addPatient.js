2f6516699a12a101cc7967b6ec84a1f4d154b504
Jen Chartier <54649817+JChartier001@users.noreply.github.com>
2019-12-24 22:00:30 -0600

    removed not needed seed files

--- data/seeds/002addPatient.js
+++ data/seeds/002addPatient.js
@@ -6,8 +6,8 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('table_name').insert([
         {mem_id: 1234, first_name: 'fake', last_name: 'name', address: '123 main st', city: 'Anywhere', state: 'US', zip: 12354, phone_number: 1234567890, d_o_b: 7/5/2001, code: true, user_id: 3},
-        {id: 2, first_name: 'alsofake', last_name: 'name', address: '123 main st', city: 'Anywhere', state: 'US', zip: 12354, phone_number: 1234567890, d_o_b: 1/5/2001, code: true, user_id: 5},
-        {id: 1, first_name: 'fake', last_name: 'name', address: '123 main st', city: 'Anywhere', state: 'US', zip: 12354, phone_number: 1234567890, d_o_b: 7/5/2001, code: true, user_id: 1}
+        {id: 22345, first_name: 'alsofake', last_name: 'name', address: '123 main st', city: 'Anywhere', state: 'US', zip: 12354, phone_number: 1234567890, d_o_b: 1/5/2001, code: true, user_id: 5},
+        {id: 154893, first_name: 'fake', last_name: 'name', address: '123 main st', city: 'Anywhere', state: 'US', zip: 12354, phone_number: 1234567890, d_o_b: 7/5/2001, code: true, user_id: 1}
         
       ]);
     });
